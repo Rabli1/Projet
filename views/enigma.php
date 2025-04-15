d81b19aacb8d4f93c080367bc8fea3783797139e
ColinDupre333 <202167407@edu.clg.qc.ca>
2025-04-15 12:43:21 -0400

    page choix enigma

--- views/enigma.php
+++ views/enigma.php
@@ -4,29 +4,27 @@ require 'partials/header.php';
 ?>
 
 <div>
-    <h2>Enigma</h2>
+    <h2 class="enigma-gains">Enigma</h2>
     <div>
-        <h3>Veuillez choisir votre mode de jeu</h3>
+        <h3 class="enigma-gains">Veuillez choisir votre mode de jeu</h3>
+        
         <div class="container-enigma-mode">
             <div class="enigma-mode">
                 <a href="/enigmaNormal" class="btn btn-primary">Normal</a>
-                <p>Dans ce mode, la question et la difficulté est aléatoire. Les récompenses sont :</p>
-                <span>
-                    Énigme difficile → 200 caps<br>
-                    Énigme moyen     → 100 caps<br>
-                    Énigme facile    → 50 caps
-                </span>
+                <p>Dans ce mode, la question et la difficulté est aléatoire.</p>
+                
             </div>
             <div class="enigma-mode">
                 <a href="/enigmaBonus" class="btn btn-primary">Bonus</a>
-                <p>Dans ce mode, la question est aléatoire, mais la difficulté est votre choix. Les récompenses sont :</p>
-                
-                Énigme difficile → 200 caps<br>
-                Énigme moyen     → 100 caps<br>
-                Énigme facile    → 50 caps
-               
+                <p>Dans ce mode, la question est aléatoire, mais la difficulté est votre choix.</p>
                 <p>Si vous répondez à 3 questions difficiles de suites un bonus de 1000 caps vous est accordé.</p>
             </div>
+        </div>
+        <div class="enigma-gains">
+            <h3>Récompenses</h3>
+            Énigme difficile → 200 caps<br>
+            Énigme moyen     → 100 caps<br>
+            Énigme facile    → 50 caps
         </div>
     </div>
 </div>
